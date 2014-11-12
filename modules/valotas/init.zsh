1f1d1e6809af68c7c24b94f961d58592199b036e
Georgios Valotasios <valotas@gmail.com>
2014-11-12 18:22:07 +0100

    Setup maven

--- modules/valotas/init.zsh
+++ modules/valotas/init.zsh
@@ -11,4 +11,10 @@ if [[ -s "$HOME/Projects/go" ]]; then
   export GOPATH="$HOME/Projects/go"
 fi
 
+if [[ -s "$HOME/usr/maven/current" ]]; then
+  export M2_HOME="$HOME/usr/maven/current";
+  export M2="$M2_HOME/bin"
+  export PATH="$PATH:$M2"
+fi
+
 export CHROME_BIN=`which chromium-browser`
