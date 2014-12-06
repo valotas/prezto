2c43f27983cc9f2f8613e1906ec6e94eea61e144
Georgios Valotasios <valotas@gmail.com>
2014-12-06 20:29:01 +0100

    Fixed the $PATH based on local installed stuff

--- modules/valotas/init.zsh
+++ modules/valotas/init.zsh
@@ -2,17 +2,27 @@ source "aliases.sh"
 
 if [[ -s "$HOME/go/current" ]]; then
   export GOROOT="$HOME/go"
-  export PATH="$PATH:$GOROOT/bin"
+  PATH="$GOROOT/bin:$PATH"
 fi
 
 if [[ -s "$HOME/Projects/go" ]]; then
   export GOPATH="$HOME/Projects/go"
 fi
 
+
+if [[ -d "$JAVA_HOME/bin" ]]; then
+  PATH="$JAVA_HOME/bin:$PATH"
+fi
+
 if [[ -s "$HOME/usr/maven/current" ]]; then
   export M2_HOME="$HOME/usr/maven/current";
   export M2="$M2_HOME/bin"
-  export PATH="$PATH:$M2"
+  PATH="$M2:$PATH"
 fi
 
+if [[ -d "$HOME/local/usr/dart/dart-sdk/bin" ]]; then
+  PATH="$HOME/local/usr/dart/dart-sdk/bin:$PATH"
+fi
+
+export PATH
 export CHROME_BIN=`which chromium-browser`
